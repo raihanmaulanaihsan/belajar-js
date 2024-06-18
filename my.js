be4d1ddc16f4cbbf0677f74e0f79ab8b5f62ec4b
raihanmaulanaihsan <raihanihsan52@gmail.com>
2024-06-17 22:12:52 -1100

    feat: menambahkan file js ke github

---
+++ my.js
@@ -0,0 +1,54 @@
+const student = [
+    {
+        nama: "pedro",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "asep",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "udin",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "jamal",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "gina",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "galang",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "sigma",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "josep",
+        umur: 17,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "sambo",
+        umur: 35,
+        hobi: ["apa aja", "rebahan"]
+    },
+    {
+        nama: "ginanjar",
+        umur: 25,
+        hobi: ["apa aja", "rebahan"]
+    }
+]
+
+console.log(student[8].nama);
